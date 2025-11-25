c101cb6f45a4246abc19c97426745b7681725cda
DavFilsDev <miharisoadavidfils@gmail.com>
2025-11-25 23:50:17 +0300

    update footer ans remove all useless content

--- frontend/src/components/common/Footer.jsx
+++ frontend/src/components/common/Footer.jsx
@@ -1,16 +1,26 @@
+import logo from '../../assets/images/logo/sopatekLogo.png'
+
 const Footer = () => {
   return (
-    <footer className="bg-gray-800 text-white py-8">
-      <div className="container mx-auto px-6">
-        <div className="flex flex-col md:flex-row justify-between items-center">
-          <div className="mb-4 md:mb-0">
-            <p>© {new Date().getFullYear()} ESN Portfolio. All rights reserved.</p>
+    <footer className="bg-dark dark:bg-light text-white">
+      <div className="flex flex-col md:flex-row justify-between items-center md:items-start gap-8 max-w-6xl mx-auto">
+        <div className="flex flex-col items-center md:items-start">
+          <div className="flex items-center">
+            <img 
+              src={logo} 
+              alt="SopaTek Portfolio Logo" 
+              className="h-16 w-auto"
+            />
+            <span className="lg:ml-3 text-2xl font-bold text-primary">SopaTek</span>
           </div>
-          <div className="flex space-x-6">
-            <a href="#" className="hover:text-primary transition-colors">Terms</a>
-            <a href="#" className="hover:text-primary transition-colors">Privacy</a>
-            <a href="#" className="hover:text-primary transition-colors">Contact</a>
-          </div>
+          <p className='text-light dark:text-dark'>© {new Date().getFullYear()} Sopatek Portfolio. All rights reserved.</p>
+        </div>
+
+        {/* Contacts */}
+        <div className="flex flex-col items-center md:items-start space-y-2">
+          <p className="font-bold text-lg text-light dark:text-dark">Contacts</p>
+          <p className='text-light dark:text-dark'>Email : contact@esn.dev</p>
+          <p className='text-light dark:text-dark'>Téléphone : +261 34 00 000 00</p>
         </div>
       </div>
     </footer>
